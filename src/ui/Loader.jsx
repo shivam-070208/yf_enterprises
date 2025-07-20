cac3019db02c06711d4994e1b9bf15e45b0a672f
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-07-20 18:35:22 +0530

    changed loader

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -3,7 +3,7 @@ import { Giflogo } from '../assets'
   const Loader = () => {
   return (
     <div className='w-screen h-screen fixed top-0 flex items-center justify-center  bg-orange-600 left-0 z-[999] '>
-        <img src={Giflogo} alt="LogoLoader" className='w-1/2' />
+        <img src={Giflogo} alt="LogoLoader" className='md:w-1/2 w-3/2' />
     </div>
   )
 }
