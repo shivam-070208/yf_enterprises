e0351f84fa44f69f352d379c0b286ef37c607359
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-07-20 20:01:38 +0530

    changes in service details

---
+++ src/assets/serviceImages.js
@@ -0,0 +1,19 @@
+// Auto-generated: Imports all images from the service folder with service1, service2, ... keys using ES6 imports
+
+import service1 from './service/1.jpg';
+import service2 from './service/2.jpg';
+import service3 from './service/3.jpg';
+import service4 from './service/4.jpg';
+import service5 from './service/5.jpg';
+import service6 from './service/6.jpg';
+
+const serviceImages = {
+  service1,
+  service2,
+  service3,
+  service4,
+  service5,
+  service6,
+};
+
+export default serviceImages;
