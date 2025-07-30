f6f657fe54ea0ff5879b7f0b78b3157415b14c53
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-07-30 13:05:35 +0530

    changes in service details

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -81,7 +81,7 @@ const AboutUs = () => {
       {/* Welcome Section */}
       <section className="py-20 bg-gray-100">
         <div className="max-w-6xl mx-auto p-5 flex flex-col lg:flex-row items-center gap-15">
-          <div className="w-full lg:w-1/2 mr-20">
+          <div className="w-full lg:w-1/2 ml-5 lg:mr-20">
             <img src={about} alt="Welcome" className="rounded-xl shadow" />
           </div>
           <div className="w-full lg:w-1/2 space-y-6">
