67cd5a0eb8d4d9f7bc54dbaa6d53c2337fc4b431
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-08-02 12:28:35 +0530

    changes in the gallery

--- src/assets/index.js
+++ src/assets/index.js
@@ -14,11 +14,35 @@ import gallery8 from './Gallery/8.jpg';
 import gallery9 from './Gallery/9.jpg';
 import gallery10 from './Gallery/10.jpg';
 import gallery11 from './Gallery/11.jpg';
+import gallery12 from './Gallery/12.jpg';
+import gallery13 from './Gallery/13.jpg';
+import gallery14 from './Gallery/14.jpg';
+import gallery15 from './Gallery/15.jpg';
+import gallery16 from './Gallery/16.jpg';
+import gallery17 from './Gallery/17.jpg';
+import gallery18 from './Gallery/18.jpg';
+import gallery19 from './Gallery/19.jpg';
+import gallery20 from './Gallery/20.jpg';
+import gallery21 from './Gallery/21.jpg';
+import gallery22 from './Gallery/22.jpg';
+import gallery23 from './Gallery/23.jpg';
+import gallery24 from './Gallery/24.jpg';
+import gallery25 from './Gallery/25.jpg';
+import gallery26 from './Gallery/26.jpg';
+import gallery27 from './Gallery/27.jpg';
+import gallery28 from './Gallery/28.jpg';
+import gallery29 from './Gallery/29.jpg';
+import gallery30 from './Gallery/30.jpg';
+import gallery31 from './Gallery/31.jpg';
+import gallery32 from './Gallery/32.jpg';
+
+
 
 
 
 // Export all gallery images as named exports.
-export const galleryImages = [gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8, gallery9, gallery10, gallery11];
+export const galleryImages = [gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8, gallery9, gallery10, gallery11, gallery12, gallery13, gallery14, gallery15, gallery16, gallery17, gallery18, gallery19, gallery20, gallery21, gallery22, gallery23, gallery24, gallery25, gallery26, gallery27, gallery28, gallery29, gallery30, gallery31, gallery32];
+
 
 
 
