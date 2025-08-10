d4650004a34bf2b5e3ad971becf0a1bf7048d434
shvgpt-070208 <shvgpt070208@gmail.com>
2025-08-10 21:58:12 +0530

    tt

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import * as compression from 'vite-plugin-compression'
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    compression({
-      algorithm: 'gzip',
-      ext: '.gz',
-    })
+   
   ],
   build: {
     rollupOptions: {
